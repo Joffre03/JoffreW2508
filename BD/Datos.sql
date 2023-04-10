7a302936f0ca5e0f515b2c15705e81f050eac716
Joffre <98908436+Joffre03@users.noreply.github.com>
2023-04-10 09:23:22 -0600

    Add files via upload

--- BD/Datos.sql
+++ BD/Datos.sql
@@ -3,4 +3,19 @@ create schema if no exists 'DBweb1' default character set utf8 collate utf8_span
 USE 'DBweb1';/*Se selecciona la base de datos para ser usada*/
 
 --Crear tabla--
-create table 
+create table `persona`(
+    `numero` int (5) not null, --se genera la columna numero con una longitud de 5 caracteres y forzozamente se necesita llenar
+    `nombre_usuario` text not null,
+    `carrera` text not null,
+    `no_cuenta` int (10) not null,
+    `direccion` text not null,
+    `telefono` varchar (8) not null,
+    `email` text not null, --puede ser texto o varchar 
+    `password` varchar (10)not null, --el varchar se puede modificar el tamaño
+    `fecha_registro` datetime not null default current_timestamp, --si no ingresas una fecha manual, agarrara la fecha del ordenador y la pone por default
+    `permisos` int (11) not null default '2' -- si no ingresas ningun permiso te dara el 2
+--valor por default va en comilla simple '
+) engine=Innodo default charset=utf8;
+alter table `persona` --seleccionamos tabla a modificar
+change column `numero` `numero` int(10) not null auto_increment --nuestra columna numero sera auto incrementable
+add primary key(`numero`)
